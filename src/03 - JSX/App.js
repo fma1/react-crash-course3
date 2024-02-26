5d696b632143b3fae22ee4fa39024abd3eb99af9
Fred Ma <fredma1234@gmail.com>
2024-02-26 16:09:24 -0500

    Finished 03 - JSX

---
+++ src/03 - JSX/App.js
@@ -0,0 +1,42 @@
+import '../App.css';
+
+function App() {
+/*
+  return (
+    <>
+      <h2>Hello World</h2>
+      <p>Test</p>
+    </>
+  );
+*/
+/*
+  const error = true;
+  return (
+    <>
+      {error && <h1>Error</h1>}
+      {!error && <h1>Error</h1>}
+    </>
+  );
+*/
+/*<input id="input" type="text" maxLength="3" />*/
+/*
+  const props = {
+    id: 'input',
+    type: 'text',
+    maxLength: '3'
+  };
+  return (
+    <>
+      <label>Input:</label>
+      <input {...props} placeholder="user" />
+    </>
+  );
+*/
+  return <p style={{
+    color: 'red',
+    textAlign: 'center',
+    fontSize: '48px'
+  }}>Hello World!</p>
+}
+
+export default App;
